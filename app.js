3d569ce0b62732c8c651071b467bb78bfbb66dd1
Dataram57 <dataram66657@gmail.com>
2023-04-18 11:13:51 +0200

    works somehow

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 //config
 const serverPort = 80;
-const wsPort = 80;
+const wsPort = 8080;
+const wsSecretKey = 'zaq1"WSX';
 //This is a tunnel
+console.clear();
 const http = require('http');
 const WebSocket = require('ws');
 
@@ -33,9 +35,73 @@ const GetMIME = (ext) => {
 //================================================================
 //#region WebSocket server
 
-const wsServer = new WebSocket.Server({port: '8080'});
+const wsServer = new WebSocket.Server({port: wsPort});
 
+const wsSessions = [];
+const wsSessionsKey = [];
+const wsSessionsFileLength = [];
+const wsSessionsFileName = [];
+const wsSessionsChunkLength = [];
+const wsSessionsResponse = [];
+//
 
+wsServer.on('connection', (socket, req) => {
+    //check key
+    if(req.headers['key'] !== wsSecretKey){
+        socket.close();
+        return;
+    }
+    console.log('New connection has been Approved');
+
+    //save session
+    socket.sessionIndex = wsSessions.push(socket) - 1;
+    wsSessionsKey.push('');
+    wsSessionsFileLength.push(-1);
+    wsSessionsFileName.push('unnamed.thing');
+    wsSessionsChunkLength.push(0);
+    wsSessionsResponse.push(null);
+
+    //define events
+
+    //on message
+    socket.on("message", message => {
+        const i = socket.sessionIndex;
+        //check status
+        if(wsSessionsFileLength[i] == -1){
+            //get setup
+            const setup = JSON.parse(message);
+            //check setup
+            console.log(setup);
+            //set parameters
+            wsSessionsFileName[i] = setup.fileName;
+            wsSessionsFileLength[i] = setup.fileSize;
+            wsSessionsChunkLength[i] = setup.chunkLength;
+            //generate the key
+            const key = Math.floor(Math.random() * 100000).toString(16);
+            console.log(key);
+            wsSessionsKey[i] = key;
+            socket.send("key;" + key);
+        }
+        else{
+            //write tunneled data to the responese
+            wsSessionsResponse[i].write(message);
+            console.log(message.toString());
+            //shrink file size
+            wsSessionsFileLength[i] -= message.length;
+            //check remaining file size
+            if(wsSessionsFileLength[i] <= 0){
+                console.log('finished writing data');
+                //end downloading
+                wsSessionsResponse[i].end();
+                //destroy ws
+                socket.close();
+                return;
+            }
+            //ask for next
+            socket.send("next;");
+        }
+    });
+});
 
 //#endregion
 
@@ -43,12 +109,43 @@ const wsServer = new WebSocket.Server({port: '8080'});
 //#region HTTP server
 
 const server = http.createServer((req, res) => {
+    const params = req.url.split('/');
+    switch(params[1]){
+        case "download":
+            if(params[2]){
+                //find the id
+                let i = wsSessions.length;
+                const key = params[2];
+                while(--i > -1)
+                    if(wsSessionsKey[i] == key)
+                        break;
+                
+                if(i > -1){
+                    //susspend key
+                    wsSessionsKey[i] == null;
+                    wsSessionsResponse[i] = res;
+                    //write headers
+                    res.setHeader('Content-Type', GetMIME(wsSessionsFileName[i].substr(wsSessionsFileName[i].lastIndexOf('.') + 1)));
+                    res.setHeader('Content-Disposition', `attachment; filename="${wsSessionsFileName[i]}"`);
+                    res.setHeader('Content-Length', wsSessionsFileLength[i]);
+                    //begin responding;
+                    wsSessions[i].send('next;');
+                }   
+                else
+                    res.write('wrong key');
+            }
+            break;
+        default:
+            res.end('Hello. This is a tunnel');
+            break;
+    }
+    /*
     const msg = "Hello. Can you read me?";
     const fileName = "test.txt";
     res.setHeader('Content-Type', GetMIME(fileName.substr(fileName.lastIndexOf('.') + 1)));
     res.setHeader('Content-Disposition', `attachment; filename="${fileName}"`);
     res.write(msg);
-    res.end();
+    */
 });
 
 server.listen(serverPort, () => {
---
+++ test.js
@@ -0,0 +1,166 @@
+//config
+const targetTunnel = 'ws://localhost:8080';
+const targetTunnelInviteLink = 'localhost/download/';
+const tunnelSecretKey = 'zaq1"WSX';
+const targetFile = 'test.txt';
+const readChunkSize = 1024;
+//This is a tunnel-tester
+console.clear();
+console.log('================================================================');
+const WebSocket = require('ws');
+const fs = require("fs");
+//start
+
+//================================================================
+//#region pseudo Main
+
+const Kill = (msg) => {
+    console.log(msg, "is demanding a Kill!!!");
+    if(ws)
+        ws.close();
+    if(fileStream)
+        if(fileStream.fd >= 0)
+            fileStream.close();
+};
+let allowCounter = 2;
+const Allow = (msg) => {
+    console.log(msg, "is ready!");
+    allowCounter--;
+    if(allowCounter == 0)
+        Start();
+};
+let state = 0;
+const Start = () => {
+    console.log("Getting the key.");
+    const setup = {
+        fileName: fileName,
+        fileSize: fileSize,
+        chunkLength: readChunkSize
+    };
+    ws.send(JSON.stringify(setup));
+};
+
+//#endregion
+
+//================================================================
+//#region FileStream
+
+//buffer which stores the data
+let fileBuffer = Buffer.alloc(readChunkSize);
+//stream is hidden inside the fs manager and it is represented by the fd (its special id)
+let fileStream;
+let fileSize = fs.statSync(targetFile).size;
+let fileName = targetFile.substring(Math.min(targetFile.lastIndexOf('..'), targetFile.lastIndexOf('/'),0));
+const loadFile = async () =>{ 
+    try {
+        fileStream = await fs.promises.open(targetFile, fs.constants.O_RDONLY | 0x10000000);
+        Allow("File");
+    } catch (error) {
+        if (error.code === 'EBUSY'){
+            console.log('file is busy');
+        } else {
+            throw error;
+        }
+    }
+};
+loadFile();
+
+//#endregion
+
+//================================================================
+//#region WebSocket
+
+const ws = new WebSocket(targetTunnel, {
+    perMessageDeflate: false,
+    headers: {
+        key: tunnelSecretKey
+    }
+});
+
+ws.on('error', event =>{
+    console.log('Wooop... there was an error with the connection.');
+});
+
+ws.on("open", event => {
+    Allow("WebSocket");
+});
+
+ws.on("message", msg => {
+    if(msg.subarray(0,4).toString() == 'key;'){
+        console.log(targetTunnelInviteLink + msg.subarray(4).toString());
+    }
+    else if(msg.subarray(0,5).toString() == 'next;'){
+        sendNextChunk();
+    }
+});
+
+ws.on('close', e => {
+    console.log("connection closed");
+    Kill();
+});
+
+//#endregion
+
+
+
+
+const sendNextChunk = () => {
+    fs.read(fileStream.fd, fileBuffer, 0, readChunkSize, null, function(err, nread) {
+        if (err) throw err;
+
+        //check if reading has ended
+        if (nread === 0) {
+            // done reading file, do any necessary finalization steps
+            fs.close(fileStream.fd, function(err) {
+                if (err) throw err;
+            });
+            return;
+        }
+
+        //prepare read chunk
+        var data;
+        if (nread < readChunkSize)
+            data = fileBuffer.slice(0, nread);
+        else
+            data = fileBuffer;
+        
+        //send chunk
+        ws.send(data);
+    });
+}
+
+
+
+
+
+/*
+
+(err, fd) => {
+        if (err) throw err;
+        function readNextChunk() {
+            fs.read(fd, fileBuffer, 0, readChunkSize, null, function(err, nread) {
+                if (err) throw err;
+
+                //check if reading has ended
+                if (nread === 0) {
+                    // done reading file, do any necessary finalization steps
+                    fs.close(fd, function(err) {
+                        if (err) throw err;
+                    });
+                    return;
+                }
+
+                var data;
+                if (nread < readChunkSize)
+                    data = fileBuffer.slice(0, nread);
+                else
+                    data = fileBuffer;
+
+                console.log(data.toString());
+            });
+        }
+        readNextChunk();
+        Allow("File");
+    }
+
+*/
